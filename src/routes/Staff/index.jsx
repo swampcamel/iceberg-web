f252b4bb421608b49fd005c44a5103473a3d14fb
Dan Mace <dmacebeta@gmail.com>
2023-07-27 10:00:37 -0700

    modify staff img, add member

--- src/routes/Staff/index.jsx
+++ src/routes/Staff/index.jsx
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion'
 import Riven from '../../assets/img/staff/riven-anime.jpg'
 import Destiny from '../../assets/img/staff/destiny-anime.jpg'
 import Kether from '../../assets/img/staff/kether-anime.jpg'
+import NyxlinNormal from '../../assets/img/staff/nyxlin-normal.jpg'
+import NyxlinAnime from '../../assets/img/staff/nyxlin-anime.jpg'
 
 import './Staff.scss'
 
@@ -21,24 +23,40 @@ const Staff = () => {
     >
         <div className = "staff-wrapper">
             <div className="member-wrapper">
-                <img src={Riven} />
+                <div className="img-wrapper">
+                    <img src={Riven} />
+                </div>
                 <div class="member-content">
                     <h3>Riven Zatoshi</h3>
                     <h4>Owner</h4>
                 </div>
             </div>
             <div className="member-wrapper">
-                <img src={Destiny} />
+                <div className="img-wrapper">
+                    <img src={Destiny} />
+                </div>
                 <div class="member-content">
                     <h3>Destiny Zatoshi</h3>
                     <h4>Co-Owner</h4>
                 </div>
             </div>
             <div className="member-wrapper">
-                <img src={Kether} />
+                <div className="img-wrapper">
+                    <img src={Kether} />
+                </div>
                 <div class="member-content">
                     <h3>Kether Malkuth</h3>
                     <h4>Manager</h4>
+                </div>
+            </div>
+            <div className="member-wrapper">
+                <div className="img-wrapper">
+                    <img className="normal-img" src={NyxlinNormal} />
+                    <img className="anime-img" src={NyxlinAnime} />
+                </div>
+                <div class="member-content">
+                    <h3>Nyxlyn Lunara</h3>
+                    <h4>Dancer</h4>
                 </div>
             </div>
         </div>
