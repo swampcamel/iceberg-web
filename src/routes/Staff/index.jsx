9906a668f47e7f58f173c36ebde33c8d4f716d61
Dan Mace <dmacebeta@gmail.com>
2023-08-03 10:50:21 -0700

    add more staff

--- src/routes/Staff/index.jsx
+++ src/routes/Staff/index.jsx
@@ -5,6 +5,12 @@ import Destiny from '../../assets/img/staff/destiny-anime.jpg'
 import Kether from '../../assets/img/staff/kether-anime.jpg'
 import NyxlinNormal from '../../assets/img/staff/nyxlin-normal.jpg'
 import NyxlinAnime from '../../assets/img/staff/nyxlin-anime.jpg'
+import DulcNormal from '../../assets/img/staff/dulcenia-normal.jpg'
+import DulcAnime from '../../assets/img/staff/dulcenia-anime.jpg'
+import KehdaNormal from '../../assets/img/staff/keyda-normal.jpg'
+import KehdaAnime from '../../assets/img/staff/keyda-anime.jpg'
+import DelbeeNormal from '../../assets/img/staff/delbee-normal.jpg'
+import DelbeeAnime from '../../assets/img/staff/delbee-anime.jpg'
 
 import './Staff.scss'
 
@@ -51,11 +57,41 @@ const Staff = () => {
             </div>
             <div className="member-wrapper">
                 <div className="img-wrapper">
+                    <img className="normal-img" src={DulcNormal} />
+                    <img className="anime-img" src={DulcAnime} />
+                </div>
+                <div class="member-content">
+                    <h3>Dulcenia Lifebringer</h3>
+                    <h4>Staff Director</h4>
+                </div>
+            </div>
+            <div className="member-wrapper">
+                <div className="img-wrapper">
+                    <img className="normal-img" src={KehdaNormal} />
+                    <img className="anime-img" src={KehdaAnime} />
+                </div>
+                <div class="member-content">
+                    <h3>Kehda Tyeres</h3>
+                    <h4>Bar Manager</h4>
+                </div>
+            </div>
+            <div className="member-wrapper">
+                <div className="img-wrapper">
                     <img className="normal-img" src={NyxlinNormal} />
                     <img className="anime-img" src={NyxlinAnime} />
                 </div>
                 <div class="member-content">
                     <h3>Nyxlyn Lunara</h3>
+                    <h4>Dancer</h4>
+                </div>
+            </div>
+            <div className="member-wrapper">
+                <div className="img-wrapper">
+                    <img className="normal-img" src={DelbeeNormal} />
+                    <img className="anime-img" src={DelbeeAnime} />
+                </div>
+                <div class="member-content">
+                    <h3>Delbee Es</h3>
                     <h4>Dancer</h4>
                 </div>
             </div>
