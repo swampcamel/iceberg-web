d574acc30d9205a87fc5d52967d143878848abae
Dan Mace <dmacebeta@gmail.com>
2023-08-09 14:34:40 -0700

    fancy up vip

--- src/routes/VIP/index.jsx
+++ src/routes/VIP/index.jsx
@@ -1,5 +1,9 @@
 import { motion } from 'framer-motion'
 
+import FrostyBG from '../../assets/img/frosty-bg.mp4'
+import BlizzyBG from '../../assets/img/blizzy-bg.mp4'
+import YetiBG from '../../assets/img/yeti-bg.mp4'
+
 import './VIP.scss'
 
 const VIP = () => {
@@ -18,38 +22,47 @@ const VIP = () => {
             >
                 <div className="vip-wrapper">
                     <div className="option-wrapper">
-                        <h3>Frosty League</h3>
-                        <h4>Nightly • 500k gil</h4>
-                        <ul>
-                            <li>Nightly Syncshell Access</li>
-                            <li>Club Shout Out</li>
-                            <li>Free Drinks (excludes Shiva)</li>
-                            <li>Increase Gamba Limit to 1 Mil</li>
-                        </ul>
+                        <video className="video-bg" src={FrostyBG} autoPlay muted loop controls='' />    
+                        <div className="option-content">
+                            <h3>Frosty League</h3>
+                            <h4>Nightly • 500k gil</h4>
+                            <ul>
+                                <li>Nightly Syncshell Access</li>
+                                <li>Club Shout Out</li>
+                                <li>Free Drinks (excludes Shiva)</li>
+                                <li>Increase Gamba Limit to 1 Mil</li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="option-wrapper">
-                        <h3>Blizzy League</h3>
-                        <h4>Monthly • 3 mil gil</h4>
-                        <ul>
-                            <li>Syncshell Access</li>
-                            <li>Free Drinks (excludes Shiva)</li>
-                            <li>250k Gamba Credit for the Month</li>
-                            <li>Discord Role</li>
-                            <li>Increase Gamba Limit to 1 Mil</li>
-                            <li>1 Hour NSFW Time with a Court of your Choice<br/>(with Court's Consent)</li>
-                        </ul>
+                        <video className="video-bg" src={BlizzyBG} autoPlay muted loop controls='' />    
+                        <div className="option-content">
+                            <h3>Blizzy League</h3>
+                            <h4>Monthly • 3 mil gil</h4>
+                            <ul>
+                                <li>Syncshell Access</li>
+                                <li>Free Drinks (excludes Shiva)</li>
+                                <li>250k Gamba Credit for the Month</li>
+                                <li>Discord Role</li>
+                                <li>Increase Gamba Limit to 1 Mil</li>
+                                <li>1 Hour NSFW Time with a Court of your Choice<br/>(with Court's Consent)</li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="option-wrapper">
-                        <h3>Yeti League</h3>
-                        <h4>6 months • 15 mil gil</h4>
-                        <ul>
-                            <li>Syncshell Access</li>
-                            <li>Free Drinks (excludes Shiva)</li>
-                            <li>500k Gamba Credit for the Month</li>
-                            <li>Discord Role</li>
-                            <li>Increase Gamba Limit to 1 Mil</li>
-                            <li>2 Hour NSFW Time with a Court of your Choice<br/>(with Court's Consent)</li>
-                        </ul>
+                        <video className="video-bg" src={YetiBG} autoPlay muted loop controls='' />    
+                        <div className="option-content">
+                            <h3>Yeti League</h3>
+                            <h4>6 months • 15 mil gil</h4>
+                            <ul>
+                                <li>Syncshell Access</li>
+                                <li>Free Drinks (excludes Shiva)</li>
+                                <li>500k Gamba Credit for the Month</li>
+                                <li>Discord Role</li>
+                                <li>Increase Gamba Limit to 1 Mil</li>
+                                <li>2 Hour NSFW Time with a Court of your Choice<br/>(with Court's Consent)</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </motion.div>
